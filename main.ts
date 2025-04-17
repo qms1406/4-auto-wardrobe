44b35bd33d1bd05ccf8dbbf6687936f875976b75
qms1406 <qms1406@gmail.com>
2025-04-17 09:59:41 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+let door = false
+let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
+        door = !(door)
+        if (door == true) {
+            strip.showColor(neopixel.colors(NeoPixelColors.White))
+            pins.servoWritePin(AnalogPin.P7, 0)
+        } else {
+            pins.servoWritePin(AnalogPin.P7, 180)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        }
+    }
+})
